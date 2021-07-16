0a2353053d3460238933f8db59b1522d625b0f81
Sam LaChapelle <sambosauce@gmail.com>
2021-07-15 21:45:02 -0400

    set up server and 1 api route

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-const express = require('express')
+//importing express and setting up the server
+const express = require("express");
+const app = express();
+//importing path
+const path = require("path");
+//importing fs
+const fs = require("fs");
+//setting the port to whatever path or 5000
+const port = process.env.PORT || 5000;
+//creating a variable to house the folder holding all .json files
+const restaurantDir = path.resolve("./restaurants");
+//setting the server to a static server to always host ./client/public unless directed otherwise
+app.use(express.static("./client/public"));
+//sending a get request to set up the route for each individual restaurant JSON object
+app.get("/api/:restaurantId", (req, res) => {
+  let filePath = path.join(restaurantDir, req.params.restaurantId + ".json");
+  res.sendFile(filePath);
+});
+//sending a get request to set up a route for the JSON object array housing all restaurant id's
+app.get("/api", (req, res) => {
+  let rests = allRestaurants();
+  let resData = JSON.stringify(rests);
+  res.type("text/json").send(resData);
+});
+
+function allRestaurants() {
+  return fs
+    .readdirSync(restaurantDir)
+    .filter((file) => file.endsWith(".json"))
+    .map((file) => JSON.parse(fs.readFileSync(path.join(restaurantDir, file))));
+}
+
+//telling the server to listen for the port and print "local server is running..."
+app.listen(port, () => {
+  console.log("local server running...");
+});
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+//importing css for App
+import "./App.css";
+//importing use Effect
+import { useEffect, useState } from "react";
+//main function App
 function App() {
+  const [restaurants, setRestaurants] = useState(null);
+  //fetching for the JSON object array holding all restaurant id's
+  useEffect(() => {
+    if (!restaurants) {
+      fetch("/api")
+        .then((res) => res.json())
+        .then((obj) => {
+          console.log(obj);
+        });
+    }
+  });
+
+  //declaring a variable assigned to the specific id entered in to the path
+  let restaurantId = document.location.pathname.split("/").splice(-1);
+  console.log(restaurantId);
+  //fetching for the specified JSON object by it's id entered in the pathname
+  fetch("/api/" + restaurantId)
+    .then((res) => res.json())
+    .then((resData) => {
+      console.log(resData);
+    });
+
   return (
-    <h1>Yelpington</h1>
+    <div>
+      <h1>Yelpington</h1>
+    </div>
   );
 }
 
